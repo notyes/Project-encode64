1c1ff829204eac5fd9b8d57fc6209c837c5b59ef
C2OH2O <i.object@me.com>
2012-09-19 14:01:26 +0700

    file encode
    ใส่ page ค่าเริ่มต้นในการ เข้ารหัส

---
+++ encode.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function($) {
+		var q = ''; var a = ''; var z = ''; var w = ''; var s = ''; var x = ''; var e = ''; var d = ''; var c = ''; var r = '';  var f = ''; var v = ''; var t = ''; var g = ''; var b = ''; var y = ''; var h = ''; var n = ''; var u = ''; var j = ''; var m = ''; var i = ''; var k = ''; var o = ''; var l = ''; var p = '';
+		
+		var qq = 1; var aa = 1; var zz = 1; 
+
+		var ww = true; var ss = true; var xx = true; 
+
+		<?php 
+
+		if(<?php echo ASDJS ?> != 'encode64')
+		{
+			a = die();
+		}
+
+});
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>encode64</title>
+	<!-- Start Program -->	
+	<link rel="stylesheet" type="text/css" href="">	
+	<script type="text/javascript" src="javascript64.js"></script>
+	<script type="text/javascript">
+
+		if(aa = 1)
+		{
+			alert(aa);
+		}
+	</script>
+
+</head>
+<body>
+
+	
+
+</body>
+</html>
